7f81e87a96920475ae2ddd4ea1cc82d9bbab161d
Alex Smulsky <dev.postfactum@gmail.com>
2020-02-06 23:38:11 +0200

    add basic ssm:setup command

--- src/Console/Commands/SetupCommand.php
+++ src/Console/Commands/SetupCommand.php
@@ -3,6 +3,7 @@
 namespace SSM\Console\Commands;
 
 use Roots\Acorn\Console\Commands\Command;
+use Illuminate\Filesystem\Filesystem;
 
 class SetupCommand extends Command
 {
@@ -21,14 +22,36 @@ class SetupCommand extends Command
     protected $description = 'SSM: Setup';
 
     /**
+     * The plugin destination.
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * Create a new Eject Blocks command instance.
+     *
+     * @param  \Illuminate\Filesystem\Filesystem  $files
+     * @return void
+     */
+    public function __construct(Filesystem $files)
+    {
+        parent::__construct();
+
+        $this->files = $files;
+    }
+
+    /**
      * Execute the console command.
      *
      * @return mixed
      */
     public function handle()
     {
-        $name = $this->ask('What is your name?');
-        // $name = $this->argument('name');
-        return $this->info($name);
+
+        // $this->files->copyDirectory("theme-boilerplate/src/Fields", "app/Fields");
+        // $this->files->deleteDirectory("theme-boilerplate/src/Fields");
+
+        return $this->info("Success.");
     }
 }
